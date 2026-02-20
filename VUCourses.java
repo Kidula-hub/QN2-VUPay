8a9146dfc130f73eeaaf450a089927447aecb84a
Kidula-hub <kidulamark2@gmail.com>
2026-02-20 19:19:29 +0300

    Add files via upload
    VUPay system

---
+++ VUCourses.java
@@ -0,0 +1,48 @@
+
+import java.util.Scanner;
+
+public class VUCourses {
+
+    public static void main(String[] args) {
+
+        String moduleCode;
+        String moduleName = " ";
+        int tuition = 0;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter Module Code (BSF, BIT, BCS, BCE): ");
+        moduleCode = input.nextLine();
+
+        moduleCode = moduleCode.toUpperCase();
+
+        if (moduleCode.equals("BSF")) {
+
+            moduleName = "BSc. Software Engineering";
+            tuition = 900000;
+
+        } else if (moduleCode.equals("BIT")) {
+
+            moduleName = "BSc. Information Technology";
+            tuition = 750000;
+
+        } else if (moduleCode.equals("BCS")) {
+
+            moduleName = "BSc. Computer Science";
+            tuition = 800000;
+
+        } else if (moduleCode.equals("BCE")) {
+
+            moduleName = "BSc. Computer Engineering";
+            tuition = 950000;
+
+        } else {
+
+            System.out.println("Wrong Module Code details");
+            return;
+        }
+
+        System.out.println("Course Name: " + moduleName);
+        System.out.println("Tuition Fee: UGX " + tuition);
+    }
+}
